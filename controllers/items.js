3f42ae079d213d25c678f368df25ed19d56cd13f
Jillian Boshart <Jilliliz@gmail.com>
2016-08-12 15:44:42 -0700

    items add to the database. If item is found with the same nname, user, and unit of measure the quantity updates. If expiration dates get implemented, the quantity update feature will be removed (as we do not update expiration dates as we update quantity).

--- controllers/items.js
+++ controllers/items.js
@@ -4,18 +4,14 @@ var passport = require('passport');
 var ItemsController = {
 
   addItems: function(request, response) {
-    Items.addItems(request.body, request.user, function(error, fridge_info) {
+    Items.addOrMakeItems(request.body, request.user, function(error, item) {
       if(error) {
         var err = new Error
         err.status = 500;
         err.error = "Error adding item to fridge."
         response.json(err)
       } else {
-        response.render('fridge', {
-          user: request.user,
-          fridge_id: fridge_info.fridge_id,
-          items: fridge_info.items
-        })
+        response.redirect('/fridge')
       }
     })
   }
--- models/items.js
+++ models/items.js
@@ -3,23 +3,33 @@ var db = app.get("db");
 
 var Items = function() {}
 
-Items.addItems = function(formData, userData, callback) {
+Items.addOrMakeItems = function(formData, userData, callback) {
   var today = new Date().toJSON().slice(0,10);
-  db.items.find({name: formData.item}, function(error, item) {
+  db.items.where("user_id=$1 AND name=$2 AND quantity_unit=$3", [userData.id, formData.item, formData.unit], function(error, item) {
     if (error) {
       callback(error, undefined)
-    } else if (!item) {
+    } else if (item.length === 0) {
       db.items.save({user_id: userData.id, fridge_id: userData.fridge_id, name: formData.item, quantity: formData.quantity, quantity_unit: formData.unit, purchase_date: today }, function(error, item) {
         if (error) {
+          console.log('make error')
           callback(error, undefined)
         } else {
+          console.log('make else')
           callback(null, item)
         }
       })
     } else {
-      callback(null, item)
+      var newQuantity = (Number(formData.quantity) + Number(item[0].quantity))
+      db.items.update({id: item[0].id, quantity: newQuantity}, function(error, item) {
+        if (error) {
+          callback(error, undefined)
+        } else {
+          console.log(item)
+          callback(null, item)
+        }
+      })
     }
   })
 }
 
-module.exports = Fridge
+module.exports = Items
