43132b1c228970c786b0d7d0957830203c933c77
Jillian Boshart <Jilliliz@gmail.com>
2016-08-16 15:13:32 -0700

    Expiration date cannot be set before today's date. Date is displayed in US format.

--- models/items.js
+++ models/items.js
@@ -4,7 +4,7 @@ var db = app.get("db");
 var Items = function() {}
 
 Items.addOrMakeItems = function(formData, userData, callback) {
-  var today = new Date().toJSON().slice(0,10);
+  var today = new Date().toISOString().slice(0,10);
 
   db.items.save({user_id: userData.id, fridge_id: userData.fridge_id, name: formData.item, quantity: formData.quantity, quantity_unit: formData.unit, purchase_date: today, expiration: formData.expiration }, function(error, item) {
     if (error) {
